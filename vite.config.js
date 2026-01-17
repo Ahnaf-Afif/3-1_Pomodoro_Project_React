3313207be501783ddb7a47afc924288550b141e5
Ahnaf-Afif <williamhens123@gmail.com>
2026-01-17 20:54:24 +0600

    vite configed

--- vite.config.js
+++ vite.config.js
@@ -4,5 +4,6 @@ import tailwindcss from "@tailwindcss/vite";
 
 // https://vite.dev/config/
 export default defineConfig({
+  base: "/3-1_Pomodoro_Project_React/",
   plugins: [react(), tailwindcss()],
 });
